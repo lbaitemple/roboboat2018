451444f3305e86427ff79e4ed017c06c49d5e201
Jay Vongphachanh <jayvong136@gmail.com>
2018-11-07 16:26:52 -0500

    Update Test_Sub.py

--- T200 ROS package/Test_Sub.py
+++ T200 ROS package/Test_Sub.py
@@ -3,9 +3,15 @@
 #!/usr/bin/env python
     import rospy
     from std_msgs.msg import String
+    from sensor_msgs.msg import Image
+ #   from Servo_Example.py import setServo HELP US
+
+
+    def Dosomething(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.height)
+    rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.width)
+    pixels = data.data
     
-    def callback(data):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
         
     def listener():
     
@@ -14,9 +20,10 @@
        # anonymous=True flag means that rospy will choose a unique
        # name for our 'listener' node so that multiple listeners can
        # run simultaneously.
-       rospy.init_node('listener', anonymous=True)
+       rospy.init_node('Motor', anonymous=True)
    
-       rospy.Subscriber("chatter", String, callback)
+       rospy.Subscriber("/zed/right/image_rect_color
+", Image, Dosomething)
    
        # spin() simply keeps python from exiting until this node is stopped
        rospy.spin()
